ddfbe980c7be5811e967123e07c8507ca624f64d
shashidhar078 <nagunurishashidhar@gmail.com>
2025-06-27 14:19:44 +0530

    successfull linking with public folder and updated path

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 
 app.use(express.static(path.join(__dirname,"/public/css")));
-
+app.use(express.static(path.join(__dirname,"/public/css")));
+app.use(express.static("public"));
 app.get("/ig/:username",(req,res)=>{
     let {username}=req.params;
     let followers=["nikhil","pruthvi","goutham"];
